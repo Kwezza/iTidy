a999916965e0a2baff6797e1903d0c7dc1255855
Kerry Thompson <kwezza@gmail.com>
2024-10-18 17:59:05 +0100

    Compiler warnings cleanup part 1

--- src/file_directory_handling.h
+++ src/file_directory_handling.h
@@ -17,6 +17,8 @@
 #include "utilities.h"
 #include "writeLog.h"
 #include "icon_misc.h"
+
+
 #include <dos/dos.h>
 
 int HasSlaveFile(char *path);
@@ -24,16 +26,15 @@ void ProcessDirectory(char *path, BOOL processSubDirs, int recursion_level);
 void GetFullPath(const char *directory, struct FileInfoBlock *fib, char *fullPath, int fullPathSize);
 int IsRootDirectorySimple(char *path);
 void removeInfoExtension(const char *input, char *output);
-int saveIconsPositionsToDisk(IconArray *iconArray);
 void SaveFolderSettings(const char *folderPath, folderWindowSize *newFolderInfo, int sanityCheck);
 void dumpIconArrayToScreen(IconArray *iconArray);
 void sanitizeAmigaPath(char *path);
 
 BOOL isDirectory(const char *path);
 BOOL does_file_or_folder_exist(const char *name, int isFile);
-BOOL GetWriteProtection(STRPTR filename);
-void SetWriteProtection(STRPTR filename, BOOL protect);
-void SetDeleteProtection(STRPTR filename, BOOL protect);
-BOOL GetDeleteProtection(STRPTR filename);
+BOOL GetWriteProtection(const char *filename);
+void SetWriteProtection(const char *filename, BOOL protect);
+void SetDeleteProtection(const char *filename, BOOL protect);
+BOOL GetDeleteProtection(const char *filename);
 
 #endif // FILE_DIRECTORY_HANDLING_H
--- src/icon_misc.h
+++ src/icon_misc.h
@@ -9,6 +9,13 @@
 #include <stdio.h>
 
 #include "main.h"
+#include "icon_management.h"
+#include "window_management.h"
+#include "file_directory_handling.h"
+#include "icon_types.h"
+#include "utilities.h"
+#include "writeLog.h"
+#include "icon_misc.h"
 
 void loadLeftOutIcons(const char *file_path); 
 void resizeLeftOutIconsArray();
